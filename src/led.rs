31da540e02638cb397eb609503ae6c086abea19b
Gabriel Dobato <dobatog@gmail.com>
2023-05-16 22:49:43 +0200

    Add basic blocking timer

--- src/led.rs
+++ src/led.rs
@@ -1,14 +1,21 @@
 //! led
 
-pub use crate::hal::gpio::{GpioExt, Output, Pin, PinState, PushPull, H8};
+use crate::hal::gpio::{Output, Pin, PinState, PushPull, H8};
 
 type DigitalOutputPin<HL, const P: char, const N: u8> = Pin<Output<PushPull>, HL, P, N>;
 pub type UserLed = DigitalOutputPin<H8, 'B', 14>;
+
+#[derive(Debug)]
+pub enum State {
+    On,
+    Off,
+}
 
 pub trait Led {
     fn set_on(&mut self);
     fn set_off(&mut self);
     fn toggle(&mut self);
+    fn get_state(&self) -> State;
 }
 
 impl Led for UserLed {
@@ -25,5 +32,12 @@ impl Led for UserLed {
     #[inline(always)]
     fn toggle(&mut self) {
         self.toggle();
+    }
+
+    fn get_state(&self) -> State {
+        match self.get_state() {
+            PinState::Low => State::Off,
+            PinState::High => State::On,
+        }
     }
 }
